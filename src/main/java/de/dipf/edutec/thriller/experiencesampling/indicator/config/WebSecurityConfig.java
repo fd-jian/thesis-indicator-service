48be88e610ad96b9e148713a67ed8c5b0f14279f
Felix Jian <felix.d.jian@gmail.com>
2020-09-17 17:56:11 +0200

    interpret password ENV variable as cleartext

--- src/main/java/de/dipf/edutec/thriller/experiencesampling/indicator/config/WebSecurityConfig.java
+++ src/main/java/de/dipf/edutec/thriller/experiencesampling/indicator/config/WebSecurityConfig.java
@@ -1,16 +1,30 @@
 package de.dipf.edutec.thriller.experiencesampling.indicator.config;
 
+import org.apache.commons.lang3.StringEscapeUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.List;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Value("${spring.security.user.name}")
+	private String username;
+
+	@Value("${spring.security.user.password}")
+	private String password;
+
+	@Value("${spring.security.user.roles}")
+	private List<String> roles;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -22,9 +36,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.and().httpBasic();
 	}
 
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	    auth.inMemoryAuthentication()
+				.withUser(username)
+				.password(passwordEncoder().encode(password))
+				.roles(roles.toArray(new String[0]));
+	}
+
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-	    return new BCryptPasswordEncoder();
+	    return new BCryptPasswordEncoder(10);
 	}
 
 }
